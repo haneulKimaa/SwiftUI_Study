4d5b808ed53830c074470d4f123dcf52390a5101
HaneulKim <khnpoq@gmail.com>
2023-05-03 23:09:08 +0900

    Chore init

---
+++ SwiftUI_Study/SwiftUI_Study/SwiftUI_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StudyApp.swift
+//  SwiftUI_Study
+//
+//  Created by 김하늘 on 2023/05/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
